30dc3cef19a63d92998922977054402f454db783
kimtaehyun05 <justinkim201@gmail.com>
2025-03-13 21:26:19 +0900

    new file:   CircleCal.py 	modified:   hello.py 	new file:   inchTocm.py.py 	new file:   kgToPound.py

---
+++ CircleCal.py
@@ -0,0 +1,10 @@
+# 원의 둘레 : 2 * pi * r
+# 원의 넓이 : pi * r * r
+# pi : 3.141592
+
+pi = 3.141592
+r = float(input("반지름을 입력하세요"))
+length = 2*r*pi
+area = r*r*pi
+print('원의 둘레',length)
+print('원의 넓이',area)
---
+++ kgToPound.py
@@ -0,0 +1,6 @@
+# 1kg = 2.20462 파운드
+
+
+kg = int(input("킬로그램 입력하세요."))
+pound = kg * 2.54
+print(kg,'kg = ',pound,'pound')
